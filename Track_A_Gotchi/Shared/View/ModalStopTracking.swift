de0406e86cf9921e9b9522787b06cfc9306cb700
LPdsgn <luigipedata.design@gmail.com>
2021-11-18 17:51:33 +0100

    Modificato bottone STOP su timetrackingview per aprire modale

---
+++ Track_A_Gotchi/Shared/View/ModalStopTracking.swift
@@ -0,0 +1,24 @@
+//
+//  ModalStopTracking.swift
+//  Track_A_Gotchi (iOS)
+//
+//  Created by Luigi Pedata on 18/11/21.
+//
+
+import SwiftUI
+
+struct ModalStopTracking: View {
+    @Binding var showModal: Bool
+    var body: some View {
+        VStack {
+            Text("Opzione 1")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            Spacer()
+            Button("Resume") {
+                self.showModal.toggle()
+            }
+        }
+    }
+}
--- Track_A_Gotchi/Shared/View/TimeTrackingScreenView.swift
+++ Track_A_Gotchi/Shared/View/TimeTrackingScreenView.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 struct TimeTrackingScreenView: View {
+    @State private var showModal = false
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            ZStack {
-                Rectangle()
-                    .frame(width: .infinity, height: 100)
-                    .foregroundColor(Color.white)
-                    .shadow(color: .gray, radius: 1, x: 0, y: 2)
-                Text ("[Task title]")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-            }
-            .offset(y:-2)
+//            ZStack {
+//                Rectangle()
+//                    .frame(width: .infinity, height: 100)
+//                    .foregroundColor(Color.white)
+//                    .shadow(color: .gray, radius: 1, x: 0, y: 2)
+//                Text ("[Task title]")
+//                    .font(.largeTitle)
+//                    .fontWeight(.bold)
+//            }
+//            .offset(y:-2)
             
             VStack(alignment: .center, spacing: 20) {
                 // 1.3 Character image (should be replaced with 3D object)
@@ -38,10 +39,14 @@ struct TimeTrackingScreenView: View {
                 Text("00:00:00")
                 Spacer()
                 // 3.3 Stop tracking button
-                Button( action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                
+                Button( action: {self.showModal.toggle()}) {
                     Image(systemName: "stop.fill")
                         .font(.system(size: 80))
                         .foregroundColor(Color.white)
+                }
+                .sheet(isPresented: $showModal){
+                    ModalStopTracking(showModal: self.$showModal)
                 }
                 .frame(width: 160, height: 160, alignment: .center)
                 .background(Circle().fill(.blue))
@@ -67,6 +72,7 @@ struct TimeTrackingScreenView: View {
             alignment: .topLeading
         )
         .background(Color.white)
+        .navigationTitle("[Task Title]")
     }
 }
 
